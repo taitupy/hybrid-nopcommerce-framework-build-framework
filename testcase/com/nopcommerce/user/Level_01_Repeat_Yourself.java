3870943de4f81dabb678d7b7181eac0efa7ef08d
DESKTOP-5UQFKP3\HP <nguyentaitupy@gmail.com>
2025-06-16 22:08:41 +0700

    Level 1: Don't Repeat yourself in programming code - Update URL

--- testcase/com/nopcommerce/user/Level_01_Repeat_Yourself.java
+++ testcase/com/nopcommerce/user/Level_01_Repeat_Yourself.java
@@ -2,14 +2,14 @@ package com.nopcommerce.user;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
-import java.util.Arrays;
 
 import java.time.Duration;
 import java.util.Random;
@@ -18,62 +18,19 @@ public class Level_01_Repeat_Yourself {
 
     private WebDriver driver;
 
-    private String firstName, lastName, emailAddress, companyName, password;
+    private String admin, employee, username, password;
 
     @BeforeClass
     public void beforeClass(){
-        ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("useAutomationExtension", false);
-        options.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
-
-        driver = new ChromeDriver(options);
-        driver.get("https://demo.nopcommerce.com/");
+        driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
-        firstName = "Thomas";
-        lastName= "Muller";
-        emailAddress = "thomasmuller" + generateRandomInt() + "@gmail.com";
-        companyName= "Automation FC";
-        password = "Taitu@123456";
-    }
-
-    @Test
-    public void TC_01_Register(){
-        driver.findElement(By.cssSelector("a.ico-register")).click();
-        driver.findElement(By.cssSelector("input#gender-male")).click();
-        driver.findElement(By.cssSelector("input#FirstName")).sendKeys(firstName);
-        driver.findElement(By.cssSelector("input#LastName")).sendKeys(lastName);
-        driver.findElement(By.cssSelector("input#Email")).sendKeys(emailAddress);
-        driver.findElement(By.cssSelector("input#Company")).sendKeys(companyName);
-        driver.findElement(By.cssSelector("input#Password")).sendKeys(password);
-        driver.findElement(By.cssSelector("input#ConfirmPassword")).sendKeys(password);
-        driver.findElement(By.cssSelector("button#register-button")).click();
-
-
-        Assert.assertEquals(driver.findElement(By.cssSelector("div.result")).getText(), "Your registration completed");
-    }
-
-    @Test
-    public void TC_02_Login(){
-        driver.findElement(By.cssSelector("a.ico-logout")).click();
-        driver.findElement(By.cssSelector("a.ico-login")).click();
-        driver.findElement(By.cssSelector("input#Email")).sendKeys(emailAddress);
-        driver.findElement(By.cssSelector("input#Password")).sendKeys(password);
-        driver.findElement(By.cssSelector("button.login-button")).click();
-
-        Assert.assertTrue(driver.findElement(By.xpath("//a[@class='ico-account' and text()='My account']")).isDisplayed());
-    }
-
-    @Test
-    public void TC_03_MyAccount(){
-        driver.findElement(By.cssSelector("a.ico-account")).click();
-
-        Assert.assertTrue(driver.findElement(By.cssSelector("input#gender-male")).isSelected());
-
-        Assert.assertEquals(driver.findElement(By.cssSelector("input#FirstName")).getAttribute("value"),firstName);
-        Assert.assertEquals(driver.findElement(By.cssSelector("input#LastName")).getAttribute("value"),lastName);
-        Assert.assertEquals(driver.findElement(By.cssSelector("input#Email")).getAttribute("value"),emailAddress);
-        Assert.assertEquals(driver.findElement(By.cssSelector("input#Company")).getAttribute("value"),companyName);
+        admin = "Admin";
+        employee= "";
+//        emailAddress = "thomasmuller" + generateRandomInt() + "@gmail.com";
+        username= "Admin";
+        password = "admin123";
     }
 
     @AfterClass
@@ -81,7 +38,31 @@ public class Level_01_Repeat_Yourself {
         driver.quit();
     }
 
-    private int generateRandomInt() {
-        return new Random().nextInt(99999);
+    @Test
+    public void TC_01_Register(){
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys(username);
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//span[text()='PIM']")).isDisplayed());
     }
+
+    @Test
+    public void TC_02_Admin() throws InterruptedException {
+        driver.findElement(By.xpath("//span[text()='Admin']")).click();
+        driver.findElement(By.xpath("//div[@class='oxd-form-row']//input[@class='oxd-input oxd-input--active']")).sendKeys(admin);
+        driver.findElement(By.xpath("//div[contains(@class, 'oxd-select-text') and contains(text(), '-- Select --')]")).click();
+        driver.findElement(By.xpath("//div[@role='option']//span[text()='Admin']")).click();
+        driver.findElement(By.xpath("//div[@class='oxd-autocomplete-text-input--before']/following-sibling::input")).sendKeys(employee);
+        driver.findElement(By.xpath("//label[@class='oxd-label' and text()='Status']/parent::div/following-sibling::div//div[@class='oxd-select-text-input']")).click();
+        driver.findElement(By.xpath("//div[@role='option']//span[text()='Enabled']")).click();
+        driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space']")).click();
+        Assert.assertEquals(driver.findElement(By.xpath("//div[contains(@class, 'orangehrm-vertical-padding')]/span")).getText(),"(1) Record Found");
+        Thread.sleep(7000);
+    }
+
+//        int generateRandomInt () {
+//            return new Random().nextInt(99999);
+//        }
+//    }
 }
