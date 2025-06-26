70025aeeb89dbc97972d83cec9d7e269d76d2d59
DESKTOP-5UQFKP3\HP <nguyentaitupy@gmail.com>
2025-06-26 21:39:48 +0700

    Build PageBase Class - Topic 02

---
+++ javaSDET/javaOOP/Dog.java
@@ -0,0 +1,12 @@
+package javaOOP;
+
+public class Dog extends Annimals{
+    public static void main(String[] args){
+        Dog dog = new Dog();
+
+        System.out.println(dog.animalAge);
+
+        dog.setAnnimalsName("Huskey");
+        System.out.println(dog.getAnnimalsName());
+    }
+}
