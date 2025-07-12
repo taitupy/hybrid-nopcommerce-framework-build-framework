c9d5f00bfa485596d8e5cb6bf7ae7f931237dee9
DESKTOP-5UQFKP3\HP <nguyentaitupy@gmail.com>
2025-07-12 10:28:57 +0700

    Multiple Browser - Build BaseTest class

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,8 +1,37 @@
 package commons;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.sql.Driver;
+import java.time.Duration;
 import java.util.Random;
 
 public class BaseTest {
+    private WebDriver driver;
+
+    protected WebDriver getBrowserDriver(String browserName){
+        switch(browserName){
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new RuntimeException("Browser is not valid.");
+        }
+
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        return driver;
+    }
+
     protected int generateRandomInt () {
         return new Random().nextInt(99999);
     }
